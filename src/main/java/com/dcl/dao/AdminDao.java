085059899702fa1275d29a66ec4d1d9683a48c12
chethan0403 <chethanhs829@gmail.com>
2026-02-24 14:53:31 +0530

    admin dao implemented new method to get all customer

--- src/main/java/com/dcl/dao/AdminDao.java
+++ src/main/java/com/dcl/dao/AdminDao.java
@@ -1,7 +1,11 @@
 package com.dcl.dao;
 
+import java.util.List;
+
 import com.dcl.dto.Admin;
+import com.dcl.dto.Customer;
 
 public interface AdminDao {
 	public boolean saveAdmin(Admin a);
+	public List<Customer> getAllCustomer();
 }
